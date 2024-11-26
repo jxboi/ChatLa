99108a43e40c3bf366f12ef11011bdd23a4f0616
“JX” <Jim_87@hotmail.com>
2024-11-26 17:59:36 +0800

    add db connection

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,35 @@
 using chatlaapp.Backend.Filters;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using chatlaapp.Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Database configuration
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (string.IsNullOrEmpty(databaseUrl))
+{
+    throw new InvalidOperationException("DATABASE_URL environment variable is not set");
+}
+
+// Parse database connection string
+var databaseUri = new Uri(databaseUrl);
+var userInfo = databaseUri.UserInfo.Split(':');
+var connectionBuilder = new NpgsqlConnectionStringBuilder
+{
+    Host = databaseUri.Host,
+    Port = databaseUri.Port,
+    Username = userInfo[0],
+    Password = userInfo[1],
+    Database = databaseUri.LocalPath.TrimStart('/'),
+    SslMode = SslMode.Require
+};
+
+// Add DbContext to services
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionBuilder.ToString()));
 
 // Add services to the container.
 builder.Services.AddControllers(options =>
